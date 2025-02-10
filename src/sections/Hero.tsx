c8fb9fe5542c8dc177cbe012ec6e043d9aed41ef
Abishek <theabishekselvan@gmail.com>
2025-02-10 12:10:59 +0530

    fix spelling mistake

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -56,7 +56,7 @@ export default function Hero() {
             </motion.div>
 
             <div className="flex justify-center">
-                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">✨ $7.5 seed round raised
+                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">✨ $7.5m seed round raised
                 </div>
             </div>
             <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">Impactful design, created effortlessly</h1>
